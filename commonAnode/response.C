8299848bb1c642a199c95ac76511cc215291c0f6
Vivek Verma <vivekverma2499@gmail.com>
2020-01-18 16:06:27 +0530

    Added responses for CommonAnode type

---
+++ commonAnode/response.C
@@ -0,0 +1,77 @@
+#include <avr/io.h>
+#include <vlanguage.h>
+
+
+void say_hellow()
+  {
+    //outputs HELLOW to the system
+    PORTA = lang[24]; //H
+    PORTB = lang[21]; //E
+    PORTC = lang[28]; //L
+    PORTD = lang[28]; //L
+    PORTD = lang[31]; //O
+    PORTD = lang[39]; //W
+  }
+
+void say_echo()
+  {
+    //outputs ECHO to the system
+    PORTA = lang[21]; //E
+    PORTB = lang[19]; //C
+    PORTC = lang[24]; //H
+    PORTD = lang[31]; //O
+  }
+
+void say_on()
+  {
+    //outputs ON to the system
+    PORTA = lang[31]; //O
+    PORTB = lang[30]; //N
+  }
+
+void say_off()
+  {
+    //outputs OFF to the system
+    PORTA = lang[31]; //O
+    PORTB = lang[22]; //F
+    PORTC = lang[22]; //F
+  }
+
+
+void say_start()
+  {
+    //outputs Start to the system
+    PORTA = lang[35]; //S
+    PORTB = lang[68]; //t
+    PORTC = lang[17]; //A
+    PORTD = lang[66]; //r
+    PORTE = lang[68]; //t
+  }
+
+
+void say_end()
+  {
+    //outputs END to the system
+    PORTA = lang[21]; //E
+    PORTB = lang[62]; //n
+    PORTC = lang[52]; //d
+  }
+
+
+void say_pause()
+  {
+    //outputs END to the system
+    PORTA = lang[32]; //P
+    PORTB = lang[17]; //A
+    PORTC = lang[37]; //U
+    PORTD = lang[35]; //S
+    PORTE = lang[21]; //E
+  }
+
+
+void say_go()
+  {
+    //outputs END to the system
+    PORTA = lang[23]; //G
+    PORTB = lang[63]; //o
+  }
